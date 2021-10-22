a837bef78021c8cc95e431087c6c6f4f2438a823
AnwarSamowar <92979286+AnwarSamowar@users.noreply.github.com>
2021-10-22 12:28:47 +0300

    Add files via upload

---
+++ Сайт/js/script.js
@@ -0,0 +1,30 @@
+console.log('hello');
+$(document).ready(function(){
+
+    $("#accordion").accordion();
+
+});
+
+let viewport = document.getElementById("container").offsetWidth;
+
+let viewslide=0;
+let slider = document.querySelector(".slider");
+
+
+function press(btn){
+    if (btn=='next'){
+        if (viewslide < 2) {
+            viewslide++;
+        } else {
+            viewslide = 0;
+        }
+       slider.style.left = -viewport * viewslide + "px";
+       
+    }
+}
+
+window.onload= function() {
+    setInterval(function() {
+    press('next');
+  }, 3000);
+}
